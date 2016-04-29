d84e151682077fe116b57b65e9b0d2c493e2d603
tgaff <tgaff@alumni.nd.edu>
2016-04-29 00:23:06 -0700

    annotate songs

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  track_number :integer
+#  album_id     :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 FactoryGirl.define do
   factory :song do
     name "Rain"
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  track_number :integer
+#  album_id     :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
