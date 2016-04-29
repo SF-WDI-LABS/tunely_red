19e473ece449db3a505cd4c7073c9ffc27c5d1be
tgaff <tgaff@alumni.nd.edu>
2016-04-28 22:08:06 -0700

    fix a failing test

--- spec/views/albums/index.html.erb_spec.rb
+++ spec/views/albums/index.html.erb_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe "albums/index", type: :view do
   before(:each) do
     assign(:albums, [
       Album.create!(
-        :name => "Name",
+        :name => "Sample Album Name",
         :artist_name => "Artist Name"
       ),
       Album.create!(
-        :name => "Name",
+        :name => "Sample Album Name",
         :artist_name => "Artist Name"
       )
     ])
@@ -16,7 +16,7 @@ RSpec.describe "albums/index", type: :view do
 
   it "renders a list of albums" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Artist Name".to_s, :count => 2
+    assert_select "span.album-name", :text => "Sample Album Name".to_s, :count => 2
+    assert_select "span.artist-name", :text => "Artist Name".to_s, :count => 2
   end
 end
