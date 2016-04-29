6c1219ef1ad47d3ed299f9c03fe52611fbc4fa52
tgaff <tgaff@alumni.nd.edu>
2016-04-28 23:33:15 -0700

    add artist drop down on album edit page

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -69,6 +69,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:name, :release_date, :artist_name)
+      params.require(:album).permit(:name, :release_date, :artist_name, :artist_id)
     end
 end
