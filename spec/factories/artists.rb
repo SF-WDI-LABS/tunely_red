b20d915a648cf09fb70b645475fcf0439cc0b9b4
tgaff <tgaff@alumni.nd.edu>
2016-04-28 22:29:38 -0700

    rails generate scaffold artist name:string image_url:string

---
+++ spec/factories/artists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :artist do
+    name "MyString"
+image_url "MyString"
+  end
+
+end
---
+++ spec/views/artists/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "artists/index", type: :view do
+  before(:each) do
+    assign(:artists, [
+      Artist.create!(
+        :name => "Name",
+        :image_url => "Image Url"
+      ),
+      Artist.create!(
+        :name => "Name",
+        :image_url => "Image Url"
+      )
+    ])
+  end
+
+  it "renders a list of artists" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Image Url".to_s, :count => 2
+  end
+end
