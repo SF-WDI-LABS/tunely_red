fc3d19e3b6d9c2c4e2f3e753c43c41d0c54c28b6
tgaff <tgaff@alumni.nd.edu>
2016-04-28 20:47:27 -0700

    add a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Album.create(name: "Shatter Me", artist_name: "Lindsey Stirling", release_date: "2014/04/29")
+
+Album.create(name: "The Piano Guys", artist_name: "Wonders", release_date: "2014/10/07")
