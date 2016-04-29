ca12282a3979437b67b1f337d45f337ede1f6115
tgaff <tgaff@alumni.nd.edu>
2016-04-28 23:08:00 -0700

    update views for album in light of artist changes

--- spec/views/albums/show.html.erb_spec.rb
+++ spec/views/albums/show.html.erb_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe "albums/show", type: :view do
   before(:each) do
     @album = assign(:album, Album.create!(
       :name => "Name",
-      :artist_name => "Artist Name"
     ))
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Artist Name/)
   end
 end
